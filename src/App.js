ac5957502f643211cf4c2e18e6fc037d15e72c2b
Arthur Baker <artbaker82@gmail.com>
2021-06-03 15:43:27 -0700

    Calculator Layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "../src/styles/styles.css";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <>
+        <div className="container my-5">
+          <div className="row">
+            <div className="col text-center">Input displayed here</div>
+          </div>
+          <div className="row">
+            <div className="col-3">
+              <button type="button" className="btn btn-info">
+                Clear
+              </button>
+            </div>
+            <div className="col-3">
+              <button type="button" className="btn btn-info">
+                +/-
+              </button>
+            </div>
+            <div className="col-3">
+              <button type="button" className="btn btn-info">
+                %
+              </button>
+            </div>
+            <div className="col-3">
+              <button type="button" className="btn btn-warning">
+                /
+              </button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-3">
+              <button type="button" className="btn btn-secondary">
+                7
+              </button>
+            </div>
+            <div className="col-3">
+              <button type="button" className="btn btn-secondary">
+                8
+              </button>
+            </div>
+            <div className="col-3">
+              <button type="button" className="btn btn-secondary">
+                9
+              </button>
+            </div>
+            <div className="col-3">
+              <button type="button" className="btn btn-warning">
+                X
+              </button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-3">
+              <button type="button" className="btn btn-secondary">
+                4
+              </button>
+            </div>
+            <div className="col-3">
+              <button type="button" className="btn btn-secondary">
+                5
+              </button>
+            </div>
+            <div className="col-3">
+              <button type="button" className="btn btn-secondary">
+                6
+              </button>
+            </div>
+            <div className="col-3">
+              <button type="button" className="btn btn-warning">
+                -
+              </button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-3">
+              <button type="button" className="btn btn-secondary">
+                1
+              </button>
+            </div>
+            <div className="col-3">
+              <button type="button" className="btn btn-secondary">
+                2
+              </button>
+            </div>
+            <div className="col-3">
+              <button type="button" className="btn btn-secondary">
+                3
+              </button>
+            </div>
+            <div className="col-3">
+              <button type="button" className="btn btn-warning">
+                +
+              </button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <button type="button" className="btn btn-secondary zero-btn">
+                0
+              </button>
+            </div>
+            <div className="col-3">
+              <button type="button" className="btn btn-secondary">
+                .
+              </button>
+            </div>
+            <div className="col-3">
+              <button type="button" className="btn btn-warning">
+                =
+              </button>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
